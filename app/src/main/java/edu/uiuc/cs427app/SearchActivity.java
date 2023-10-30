c4208c3bc9c070f3daf3848505237f2570e0d3e4
unknown <lo17@illinois.edu>
2023-10-30 09:54:08 +0900

    added themes to SearchActivity and a backButton to go back to MainActivity from SearchActivity

--- app/src/main/java/edu/uiuc/cs427app/SearchActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/SearchActivity.java
@@ -4,15 +4,21 @@ import static android.content.ContentValues.TAG;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -39,29 +45,100 @@ import com.google.android.libraries.places.api.net.FetchPlaceResponse;
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
 import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
 
     private PlacesClient placesClient;
     private AutocompleteSessionToken sessionToken;
     private PlacesAdapter adapter;
     private EditText editSearch;
     private ProgressBar progressBar;
-
+    private static int themeColor1;
+    private static int themeColor2;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseAuth auth;
+    FirebaseUser user;
+    String userTheme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("MainActivity","onCreate");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        if (user != null) {
+            DocumentReference userDocRef = db.collection("users").document(user.getUid());
+            userDocRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // Retrieve user theme from the document
+                        userTheme = document.getString("userTheme");
+                        if (userTheme != null) {
+                            Log.i("RetrievedUser",userTheme);
+                            changeTheme.setTheme(this, userTheme);
+                            Window window1 = getWindow();
+                            switch (userTheme) {
+                                case "theme1":
+                                    themeColor1 = Color.parseColor("#FF6200EE");
+                                    themeColor2 = Color.parseColor("#FF3700B3");
+                                    break;
+                                case "theme2":
+                                    themeColor1 = Color.parseColor("#FF903A");
+                                    themeColor2 = Color.parseColor("#FFBB86");
+                                    break;
+                                case "theme3":
+                                    themeColor1 = Color.parseColor("#b46b41");
+                                    themeColor2 = Color.parseColor("#cd9575");
+                                    break;
+                                case "theme4":
+                                    themeColor1 = Color.parseColor("#3aa9ff");
+                                    themeColor2 = Color.parseColor("#86caff");
+                                    break;
+                                default:
+                                    themeColor1 = Color.parseColor("#FF6200EE");
+                                    themeColor2 = Color.parseColor("#FF3700B3");
+                                    break;
+                            }
+                            getSupportActionBar().setBackgroundDrawable(new ColorDrawable(themeColor1));
+                            window1.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                            window1.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                            window1.setStatusBarColor(themeColor2);
+                            getSupportActionBar().setDisplayShowTitleEnabled(false);
+                            getSupportActionBar().setDisplayShowTitleEnabled(true);
+                            setupUI();
+                        } else {
+                        }
+                    } else {
+                    }
+                } else {
+                    Log.e("Firestore", "Error getting user document", task.getException());
+                }
+            });
+        } else {
+            Log.e("Firestore", "User not logged in");
+        }
 
+        setupUI();
+    }
+
+    private void setupUI() {
+        setContentView(R.layout.activity_search);
         String apiKey = BuildConfig.PLACES_API_KEY;
 
         if (!Places.isInitialized()) {
             Places.initialize(this, apiKey);
         }
+        Button backButton = findViewById(R.id.button9);
+        backButton.setOnClickListener(this);
 
         placesClient = Places.createClient(this);
         sessionToken = AutocompleteSessionToken.newInstance();
@@ -92,9 +169,7 @@ public class SearchActivity extends AppCompatActivity {
                 return false;
             }
         });
-
     }
-
     private void searchPlaces() {
 
         progressBar.setVisibility(View.VISIBLE);
@@ -218,5 +293,14 @@ public class SearchActivity extends AppCompatActivity {
             return v;
         }
 
+    }
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()) {
+            case R.id.button9:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
