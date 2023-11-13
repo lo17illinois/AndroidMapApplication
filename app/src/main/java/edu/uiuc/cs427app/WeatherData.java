66af2457051ea7b5c44739f340cb71488da8db1b
Kshitij6798 <46404445+Kshitij6798@users.noreply.github.com>
2023-11-12 21:08:05 -0600

    Added Comments

--- app/src/main/java/edu/uiuc/cs427app/WeatherData.java
+++ app/src/main/java/edu/uiuc/cs427app/WeatherData.java
@@ -4,6 +4,9 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+/**
+ * Represents weather data containing information about weather conditions, and wind conditions.
+ */
 
 public class WeatherData {
     @SerializedName("main")
