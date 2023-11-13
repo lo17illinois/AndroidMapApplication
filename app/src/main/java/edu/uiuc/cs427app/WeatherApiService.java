7fbbb0818a108a96222d2bcc7d9a329d8d1e6f20
Kshitij6798 <46404445+Kshitij6798@users.noreply.github.com>
2023-11-12 21:04:50 -0600

    Added Comments

--- app/src/main/java/edu/uiuc/cs427app/WeatherApiService.java
+++ app/src/main/java/edu/uiuc/cs427app/WeatherApiService.java
@@ -4,7 +4,18 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
+/**
+ * Interface for defining the Weather API service using Retrofit.
+ */
+
 public interface WeatherApiService {
+        /**
+     * Gets weather data for a specific city.
+     *
+     * @param city    The name of the city for which weather data is requested.
+     * @param apiKey  The API key for accessing the weather data.
+     * @return        A Retrofit Call object containing the WeatherData for the specified city.
+     */
     @GET("weather")
     Call<WeatherData> getWeatherData(@Query("q") String city, @Query("appid") String apiKey);
 }
