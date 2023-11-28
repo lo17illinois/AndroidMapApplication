660b611472e3ca00b7a197a3ac425f2ddfb3780d
Nithin Nathan <nithinnathan@Nithins-MacBook-Pro-2.local>
2023-11-28 00:44:37 -0600

    added test for adding duplicate cities (i.e. adding chicago twice)

--- app/src/androidTest/java/edu/uiuc/cs427app/AddRemoveTest.java
+++ app/src/androidTest/java/edu/uiuc/cs427app/AddRemoveTest.java
@@ -31,6 +31,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+// tests functionality of adding/removing a city
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class AddRemoveTest {
@@ -39,14 +40,16 @@ public class AddRemoveTest {
     public ActivityScenarioRule<LoginActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(LoginActivity.class);
 
-    //Allows time for UI to be setup
+    // allows time for UI to be setup
     @Before
     public void setUp() {
         IdlingPolicies.setMasterPolicyTimeout(20, TimeUnit.SECONDS);
         IdlingPolicies.setIdlingResourceTimeout(20, TimeUnit.SECONDS);
     }
+
     @Test
     public void addRemoveTest() {
+
         // type in credentials and login
         onView(withId(R.id.username)).perform(typeText("add_remove_test"), closeSoftKeyboard());
         onView(withId(R.id.password)).perform(typeText("add_remove_test"), closeSoftKeyboard());
@@ -58,7 +61,7 @@ public class AddRemoveTest {
         onView(withId(R.id.spinner)).perform(click());
         Espresso.unregisterIdlingResources(idlingResource1);
 
-        // assert that all list items (just "Select a City") are present, and click on select a city
+        // assert that all list items are present (just "Select a City"), and click on select a city
         IdlingResource idlingResource2 = new ElapsedTimeIdlingResource(20);
         Espresso.registerIdlingResources(idlingResource2);
         List<String> expectedEmptyList = Arrays.asList("Select a City");
@@ -82,27 +85,18 @@ public class AddRemoveTest {
         Espresso.unregisterIdlingResources(idlingResource3);
 
         // type in chicago, click add button, and then click back button
-        // type chicago
         IdlingResource idlingResource4 = new ElapsedTimeIdlingResource(20);
         Espresso.registerIdlingResources(idlingResource4);
         onView(withId(R.id.nameInput)).perform(typeText("Chicago"), closeSoftKeyboard());
         Espresso.unregisterIdlingResources(idlingResource4);
-        // add
-//        IdlingResource idlingResource5 = new ElapsedTimeIdlingResource(20);
-//        Espresso.registerIdlingResources(idlingResource5);
         onView(withId(R.id.button)).perform(click());
-//        Espresso.unregisterIdlingResources(idlingResource5);
-        // back
         IdlingResource idlingResource6 = new ElapsedTimeIdlingResource(20);
         Espresso.registerIdlingResources(idlingResource6);
         onView(withId(R.id.button9)).perform(click());
         Espresso.unregisterIdlingResources(idlingResource6);
 
         // click on location drop down
-//        IdlingResource idlingResource7 = new ElapsedTimeIdlingResource(20);
-//        Espresso.registerIdlingResources(idlingResource7);
         onView(withId(R.id.spinner)).perform(click());
-//        Espresso.unregisterIdlingResources(idlingResource7);
 
         // assert that all locations are present and select chicago
         IdlingResource idlingResource8 = new ElapsedTimeIdlingResource(20);
@@ -120,6 +114,43 @@ public class AddRemoveTest {
                 .inRoot(isPlatformPopup())
                 .perform(click());
         Espresso.unregisterIdlingResources(idlingResource8);
+
+        // test to see that adding chicago twice doesn't actually add a duplicate to the list of cities. first click on add location button
+        IdlingResource idlingResource20 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource20);
+        onView(withId(R.id.buttonAddLocation)).perform(click());
+        Espresso.unregisterIdlingResources(idlingResource20);
+
+        // type in chicago, click add button, and then click back button
+        IdlingResource idlingResource21 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource21);
+        onView(withId(R.id.nameInput)).perform(typeText("Chicago"), closeSoftKeyboard());
+        Espresso.unregisterIdlingResources(idlingResource21);
+        onView(withId(R.id.button)).perform(click());
+        IdlingResource idlingResource23 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource23);
+        onView(withId(R.id.button9)).perform(click());
+        Espresso.unregisterIdlingResources(idlingResource23);
+
+        // click on location drop down
+        onView(withId(R.id.spinner)).perform(click());
+
+        // assert that all locations are present and select chicago
+        IdlingResource idlingResource22 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource22);
+        List<String> expectedChicagoList2 = Arrays.asList("Select a City", "Chicago");
+        for (String item : expectedChicagoList2) {
+            onData(allOf(is(instanceOf(String.class)), is(item))).inRoot(isPlatformPopup()).check((view, noViewFoundException) -> {
+                if (noViewFoundException != null) {
+                    throw noViewFoundException;
+                }
+                assertThat(view, notNullValue());
+            });
+        }
+        onData(allOf(is(instanceOf(String.class)), is("Chicago")))
+                .inRoot(isPlatformPopup())
+                .perform(click());
+        Espresso.unregisterIdlingResources(idlingResource22);
 
         // click on delete for chicago
         IdlingResource idlingResource9 = new ElapsedTimeIdlingResource(20);
@@ -149,20 +180,11 @@ public class AddRemoveTest {
                 .perform(click());
         Espresso.unregisterIdlingResources(idlingResource11);
 
-        // click on add location and add Urbana and Champaign
-//        IdlingResource idlingResource12 = new ElapsedTimeIdlingResource(20);
-//        Espresso.registerIdlingResources(idlingResource12);
+        // click on add location and add urbana
         onView(withId(R.id.buttonAddLocation)).perform(click());
-//        Espresso.unregisterIdlingResources(idlingResource12);
-        // add urbana
-//        IdlingResource idlingResource13 = new ElapsedTimeIdlingResource(20);
-//        Espresso.registerIdlingResources(idlingResource13);
         onView(withId(R.id.nameInput)).perform(typeText("Urbana"), closeSoftKeyboard());
-//        Espresso.unregisterIdlingResources(idlingResource13);
-//        IdlingResource idlingResource14 = new ElapsedTimeIdlingResource(20);
-//        Espresso.registerIdlingResources(idlingResource14);
         onView(withId(R.id.button)).perform(click());
-//        Espresso.unregisterIdlingResources(idlingResource14);
+
         // add champaign
         IdlingResource idlingResource15 = new ElapsedTimeIdlingResource(20);
         Espresso.registerIdlingResources(idlingResource15);
@@ -172,6 +194,7 @@ public class AddRemoveTest {
         Espresso.registerIdlingResources(idlingResource16);
         onView(withId(R.id.button)).perform(click());
         Espresso.unregisterIdlingResources(idlingResource16);
+
         // go back
         IdlingResource idlingResource17 = new ElapsedTimeIdlingResource(20);
         Espresso.registerIdlingResources(idlingResource17);
@@ -202,20 +225,20 @@ public class AddRemoveTest {
         Espresso.unregisterIdlingResources(idlingResource19);
 
         // remove urbana
-        IdlingResource idlingResource20 = new ElapsedTimeIdlingResource(20);
-        Espresso.registerIdlingResources(idlingResource20);
+        IdlingResource idlingResource29 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource29);
         onView(withId(R.id.buttonRemoveLocation)).perform(click());
-        Espresso.unregisterIdlingResources(idlingResource20);
+        Espresso.unregisterIdlingResources(idlingResource29);
 
         // show list items
-        IdlingResource idlingResource21 = new ElapsedTimeIdlingResource(20);
-        Espresso.registerIdlingResources(idlingResource21);
+        IdlingResource idlingResource30 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource30);
         onView(withId(R.id.spinner)).perform(click());
-        Espresso.unregisterIdlingResources(idlingResource21);
+        Espresso.unregisterIdlingResources(idlingResource30);
 
         // assert that only urbana was removed and click on champaign
-        IdlingResource idlingResource22 = new ElapsedTimeIdlingResource(20);
-        Espresso.registerIdlingResources(idlingResource22);
+        IdlingResource idlingResource31 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource31);
         List<String> expectedChampaignList = Arrays.asList("Select a City", "Champaign");
         for (String item : expectedChampaignList) {
             onData(allOf(is(instanceOf(String.class)), is(item))).inRoot(isPlatformPopup()).check((view, noViewFoundException) -> {
@@ -228,13 +251,13 @@ public class AddRemoveTest {
         onData(allOf(is(instanceOf(String.class)), is("Champaign")))
                 .inRoot(isPlatformPopup())
                 .perform(click());
-        Espresso.unregisterIdlingResources(idlingResource22);
+        Espresso.unregisterIdlingResources(idlingResource31);
 
         // remove champaign
-        IdlingResource idlingResource23 = new ElapsedTimeIdlingResource(20);
-        Espresso.registerIdlingResources(idlingResource23);
+        IdlingResource idlingResource32 = new ElapsedTimeIdlingResource(20);
+        Espresso.registerIdlingResources(idlingResource32);
         onView(withId(R.id.buttonRemoveLocation)).perform(click());
-        Espresso.unregisterIdlingResources(idlingResource23);
+        Espresso.unregisterIdlingResources(idlingResource32);
 
         // show list
         IdlingResource idlingResource24 = new ElapsedTimeIdlingResource(20);
@@ -242,7 +265,7 @@ public class AddRemoveTest {
         onView(withId(R.id.spinner)).perform(click());
         Espresso.unregisterIdlingResources(idlingResource24);
 
-        // assert that champaign was removed, should just be the prompt now. Click on select a city
+        // assert that champaign was removed (should just be the prompt now), click on select a city
         IdlingResource idlingResource25 = new ElapsedTimeIdlingResource(20);
         Espresso.registerIdlingResources(idlingResource25);
         for (String item : expectedEmptyList) {
@@ -270,7 +293,7 @@ public class AddRemoveTest {
         onView(withId(R.id.spinner)).perform(click());
         Espresso.unregisterIdlingResources(idlingResource27);
 
-        // assert that select a city was NOT removed, and click on it
+        // assert that select a city was not removed and click on it
         IdlingResource idlingResource28 = new ElapsedTimeIdlingResource(20);
         Espresso.registerIdlingResources(idlingResource28);
         for (String item : expectedEmptyList) {
